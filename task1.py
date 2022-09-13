0810ff702fb719c41612ceb662c45de9b012a27d
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-09-13 12:35:05 +0300

    v002

--- task1.py
+++ task1.py
@@ -1,6 +1,30 @@
-# 1 - Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр. Учтите, что числа могут быть отрицательными
+# 1 - Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Учтите, что числа могут быть отрицательными
 
 # Пример:
 
 # 67.82 -> 23
 # 0.56 -> 11
+
+def InputNumbers():
+    index = True
+    while index:
+        try:
+            number = float(input("Введите вещественное число : "))
+            index = False
+        except ValueError:
+            print("Это не вещественное число.")
+    return number
+
+
+def SumNumbers(num):
+    sumNumber = 0
+    num = str(num)
+    for i in num:
+        if i != '.' and i != '-':
+            sumNumber = sumNumber + int(i)
+    return sumNumber
+
+
+number = InputNumbers()
+print(f"{number} -> {SumNumbers(number)}")
--- task2.py
+++ task2.py
@@ -4,3 +4,26 @@
 
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def checkNumber(num):
+    if 6 <= num <= 7:
+        print("Yes")
+    elif 0 < num < 6:
+        print("No")
+    else:
+        print("число вне пределов 7 дней")
+
+
+num = InputNumbers("Введите число: ")
+checkNumber(num)
